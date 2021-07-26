8369cd926b251655d3be029fc8cc21830c4ca6af
acerix <dylan@psilly.com>
2021-07-26 03:02:14 -0400

    Export π

--- src/browser.ts
+++ src/browser.ts
@@ -9,6 +9,14 @@ import Rat from './Rat'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
 (window as any).Rat = Rat
 
+import Polyrat from './Polyrat'
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+(window as any).Polyrat = Polyrat
+
+import {π} from './Rat'
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+(window as any).π = π
+
 // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513739/5433572
 // @todo can I?
 // declare global {
