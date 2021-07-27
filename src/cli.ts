ba2c635ab13f6afba6b18f348e379a446e3841ef
acerix <dylan@psilly.com>
2021-07-26 23:51:13 -0400

    Fix CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
-import cnum, {Rat} from './cnum.js'
-// const {Rat} = require('cnum')
+import cnum from './cnum'
 
 // if there are args, evaluate as an expression and return the result
 if (process.argv.length > 2) {
   const engine = process.argv.shift()
+  void(engine)
   const script = process.argv.shift()
+  void(script)
   const input = process.argv.join(' ')
-  console.log('E', engine, 'S', script, 'I', input)
+  console.log(cnum.evaluate(input))
 }
 // otherwise, prompt for expressions to evaluate
 else {
-  console.log(`cnum v${cnum.version}`, Rat)
+  console.log(`cnum v${cnum.version}`)
   const prompt = 'cnum> '
   const stdin = process.openStdin()
   stdin.setEncoding('utf8')
   process.stdout.write(prompt)
   stdin.on('data', function (result) {
-    result = 'poop'
-    cnum.evaluate(result)
+    console.log(cnum.evaluate(result))
     process.stdout.write(prompt)
   })
 }
