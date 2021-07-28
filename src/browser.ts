1381fe25c1075ed433de4704333f0c973a166aba
acerix <dylan@psilly.com>
2021-07-27 21:43:06 -0400

    Fix undef

--- src/browser.ts
+++ src/browser.ts
@@ -13,10 +13,6 @@ import Polyrat from './Polyrat'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
 (window as any).Polyrat = Polyrat
 
-import {π} from './Rat'
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-(window as any).π = π
-
 // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513739/5433572
 // @todo can I?
 // declare global {
